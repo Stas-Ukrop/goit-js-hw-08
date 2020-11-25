6b3329cc730b93478175e3dcfa3c29a61cbd983b
Stas Chapanskiyi <stkr.stkr@gmail.com>
2020-11-25 20:10:31 +0200

    change bug

--- js/js-1.js
+++ js/js-1.js
@@ -1,9 +1,8 @@
 import galleryItems  from './gallery-items.js';
 const gallery=document.querySelector('ul');
-let corentNum=0;
-let newArr=galleryItems.map(({description,preview,original})=>{
+let newArr=galleryItems.map(({description,preview,original},index)=>{
   return `<li class="gallery__item"><a class="gallery__link" href=${original}><img class="gallery__image"
-   src=${preview} data-source=${original} data-name=${corentNum++} alt=${description}/></a></li>`;
+   src=${preview} data-source=${original} data-name=${index} alt=${description}/></a></li>`;
 }).join('');
 
 gallery.innerHTML=newArr;
--- js/js-lightbox.js
+++ js/js-lightbox.js
@@ -1,36 +1,44 @@
-const jsLightbox =document.querySelector('ul[class="gallery js-gallery"]');
-const lightboxImage=document.querySelector('img[class="lightbox__image"]');
-const buttonClose=document.querySelector('button[class="lightbox__button"]');
-const lightbox=document.querySelector('div[class="lightbox js-lightbox"]');
-const img=document.querySelectorAll('img[data-name]');
-const boby=document.body;
-const oldHttp=lightboxImage.src;
+(function (){
+const jsLightbox = document.querySelector('ul[class="gallery js-gallery"]');
+const lightboxImage = document.querySelector('img[class="lightbox__image"]');
+const buttonClose = document.querySelector('button[class="lightbox__button"]');
+const lightbox = document.querySelector('div[class="lightbox js-lightbox"]');
+const img = document.querySelectorAll("img[data-name]");
+const body = document.body;
+const oldHttp = lightboxImage.src;
 let i;
 
-boby.addEventListener('keydown',(event)=>{
-    if(event.keyCode==39){
-        (i==img.length-1)?i=0:i++;
-        lightboxImage.src=img[i].dataset.source;
-    }else if(event.keyCode==37){
-        (i<1)?i=img.length-1:i--;
-        lightboxImage.src=img[i].dataset.source;
-    }else if(event.keyCode==27){
-        closeWind(event);
-    }
-});
-    
-jsLightbox.addEventListener('click',(event)=>{
+jsLightbox.addEventListener("click", (event) => {
+  if(event.target.nodeName=='IMG'){
     event.preventDefault();
-    lightboxImage.src=oldHttp;
-    lightbox.classList.add('is-open');     
-    lightboxImage.src=event.target.dataset.source;
-    i=Number(event.target.dataset.name);
+    lightboxImage.src = oldHttp;
+    lightbox.classList.add("is-open");
+    lightboxImage.src = event.target.dataset.source;
+    i = Number(event.target.dataset.name);
+    body.addEventListener('keydown',func);
+    }  
 });
-buttonClose.addEventListener('click',(event)=>{
-    closeWind(event);
+
+buttonClose.addEventListener("click", (event) => {
+  closeWind(event);
 });
-function closeWind(event){
-    event.preventDefault();
-    lightbox.classList.remove('is-open');     
-    lightboxImage.src=oldHttp;
+
+function closeWind(event) {
+  event.preventDefault();
+  lightbox.classList.remove("is-open");
+  lightboxImage.src = oldHttp;
 }
+
+function func(event) {
+  if (event.keyCode == 39) {
+    i == img.length - 1 ? (i = 0) : i++;
+    lightboxImage.src = img[i].dataset.source;
+  } else if (event.keyCode == 37) {
+    i < 1 ? (i = img.length - 1) : i--;
+    lightboxImage.src = img[i].dataset.source;
+  } else if (event.keyCode == 27) {
+    closeWind(event);
+    body.removeEventListener('keydown',func(event));
+  }  
+};
+})();
